9c4b9527302f3b87d6bef1c9956fb5bfae045b20
zhur4k <kostya.zhurovich12052@gmail.com>
2024-12-20 10:13:22 +0300

    feat:create global exception handler.

---
+++ src/main/java/com/library/libraryapi/exception/GlobalExceptionHandler.java
@@ -0,0 +1,15 @@
+package com.library.libraryapi.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleAllExceptions(Exception exception) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(exception.getMessage());
+    }
+}
