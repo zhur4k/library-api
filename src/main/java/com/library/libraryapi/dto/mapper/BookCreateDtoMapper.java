3b1176d8ab605345da1c700fe6d2240190df9bfb
zhur4k <kostya.zhurovich12052@gmail.com>
2024-12-20 10:04:03 +0300

    feat:create dtos and mappers.

--- src/main/java/com/library/libraryapi/dto/mapper/BookCreateDtoMapper.java
+++ src/main/java/com/library/libraryapi/dto/mapper/BookCreateDtoMapper.java
@@ -2,9 +2,11 @@ package com.library.libraryapi.dto.mapper;
 
 import com.library.libraryapi.dto.BookCreateDto;
 import com.library.libraryapi.model.Book;
+import org.springframework.stereotype.Component;
 
 import java.util.function.Function;
 
+@Component
 public class BookCreateDtoMapper implements Function<BookCreateDto, Book> {
     @Override
     public Book apply(BookCreateDto bookCreateDto) {
--- src/main/java/com/library/libraryapi/dto/mapper/BookUpdateDtoMapper.java
+++ src/main/java/com/library/libraryapi/dto/mapper/BookUpdateDtoMapper.java
@@ -2,9 +2,11 @@ package com.library.libraryapi.dto.mapper;
 
 import com.library.libraryapi.dto.BookUpdateDto;
 import com.library.libraryapi.model.Book;
+import org.springframework.stereotype.Component;
 
 import java.util.function.Function;
 
+@Component
 public class BookUpdateDtoMapper implements Function<BookUpdateDto, Book> {
     @Override
     public Book apply(BookUpdateDto bookUpdateDto) {
